a95b0628ac3335ceac1d6dcd46338c69b02ee576
AnPT173 <phanthienan01072003@gmail.com>
2023-09-26 21:27:56 +0700

    add staff, fix scheduleData

--- src/utils/mock-data/schedule.js
+++ src/utils/mock-data/schedule.js
@@ -1,41 +1,115 @@
-
-
-const cageIds = ['CA001', 'CA002', 'CA003', 'CA004', 'CA005'];
-const foodTypes = ['Product 1', 'Product 2', 'Product 3'];
-
-
-function randomDate(start, end) {
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-}
-
-
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-const scheduleData = [];
-
-for (let i = 0; i < 10; i++) {
-  const cageId = cageIds[Math.floor(Math.random() * cageIds.length)];
-  const foodType = foodTypes[Math.floor(Math.random() * foodTypes.length)];
-  const startDate = randomDate(new Date(2023, 0, 1), new Date(2023, 11, 31));
-  const startHour = randomNumber(0, 23);
-  const endDate = new Date(startDate);
-  endDate.setDate(startDate.getDate() + randomNumber(1, 7)); 
-  const endHour = randomNumber(0, 23);
-  const foodQuantity = randomNumber(1, 10); 
-
-  scheduleData.push({
-    taskTitle: `Feed birds in ${cageId}`,
-    foodType,
-    startDate,
-    startHour,
-    endDate,
-    endHour,
-    foodQuantity,
-  });
-}
-
-
-console.log(scheduleData);
+const scheduleData = [
+    {
+      cageId: 'CA001',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 1',
+      date: '2023-10-01',
+      startHour: '08:00 AM',
+      endHour: '08:30 AM',
+      foodQuantity: '100 grams',
+      staffId: 'STA001',
+      status: 'Completed'
+    },
+    {
+      cageId: 'CA001',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 2',
+      date: '2023-10-02',
+      startHour: '08:15 AM',
+      endHour: '08:45 AM',
+      foodQuantity: '120 grams',
+      staffId: 'STA001',
+      status: 'Complete'
+    },
+    {
+      cageId: 'CA002',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 1',
+      date: '2023-10-01',
+      startHour: '09:00 AM',
+      endHour: '09:30 AM',
+      foodQuantity: '90 grams',
+      staffId: 'STA002',
+      status: 'Complete'
+    },
+    {
+      cageId: 'CA002',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 3',
+      date: '2023-10-02',
+      startHour: '09:15 AM',
+      endHour: '09:45 AM',
+      foodQuantity: '110 grams',
+      staffId: 'STA002',
+      status: 'Pending'
+    },
+    {
+      cageId: 'CA003',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 2',
+      date: '2023-10-01',
+      startHour: '10:00 AM',
+      endHour: '10:30 AM',
+      foodQuantity: '80 grams',
+      staffId: 'STA003',
+      status: 'Late'
+    },
+    {
+      cageId: 'CA003',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 1',
+      date: '2023-10-02',
+      startHour: '10:15 AM',
+      endHour: '10:45 AM',
+      foodQuantity: '95 grams',
+      staffId: 'STA003',
+      status: 'Late'
+    },
+    {
+      cageId: 'CA004',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 3',
+      date: '2023-10-01',
+      startHour: '11:00 AM',
+      endHour: '11:30 AM',
+      foodQuantity: '110 grams',
+      staffId: 'STA004',
+      status: 'Complete'
+    },
+    {
+      cageId: 'CA004',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 2',
+      date: '2023-10-02',
+      startHour: '11:15 AM',
+      endHour: '11:45 AM',
+      foodQuantity: '85 grams',
+      staffId: 'STA004',
+      status: 'Complete'
+    },
+    {
+      cageId: 'CA005',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 1',
+      date: '2023-10-01',
+      startHour: '12:00 PM',
+      endHour: '12:30 PM',
+      foodQuantity: '100 grams',
+      staffId: 'STA005',
+      status: 'Complete'
+    },
+    {
+      cageId: 'CA005',
+      taskTitle: 'Feed Bird',
+      foodType: 'Product 3',
+      date: '2023-10-02',
+      startHour: '12:15 PM',
+      endHour: '12:45 PM',
+      foodQuantity: '120 grams',
+      staffId: 'STA005',
+      status: 'Pending'
+    },
+  ];
+  
+  
+  
