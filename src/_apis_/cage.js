3a56b3e96aaeff091e029498e65920f373269eb3
Dang.Vu <dangvmse63447@fpt.edu.vn>
2023-10-15 12:21:33 +0700

    remove unuse

--- src/_apis_/cage.js
+++ src/_apis_/cage.js
@@ -1,9 +1,9 @@
 import { cagesData } from 'src/utils/mock-data/cage'
-import mock from './mock'
 import mockData from 'src/utils/mock-data'
 import { birdsData } from 'src/utils/mock-data/bird'
+import mock from './mock'
 
-let CageCard = cagesData
+const CageCard = cagesData
 
 mock.onGet('/api/cage/cards').reply(200, { CageCard})
 
