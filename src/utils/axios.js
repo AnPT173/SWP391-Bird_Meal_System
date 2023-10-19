ff733f651f5f06d26d7505e251dbf9bc71bcf706
Dang.Vu <dangvmse63447@fpt.edu.vn>
2023-10-19 23:22:45 +0700

    correct base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // ----------------------------------------------------------------------
 
-const BASE_URL = 'localhost:8080/api';
+const BASE_URL = 'http://localhost:8080/api';
 // const axiosInstance = axios.create();
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 
